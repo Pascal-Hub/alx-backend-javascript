7cd51e9e5258044b4f3b63c69a80ac4be3d42250
Pascal-Hub <sequencepascal@gmail.com>
2023-10-02 06:42:10 -0700

    unittest

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,16 +1,26 @@
+const { describe, it } = require('mocha');
 // const express = require('express');
 const { expect } = require('chai');
 const app = require('./api');
 const request = require('request');
 
 describe('API integration test', () => {
-  const API_URL = 'http://localhost:7865';
-
+  const API_URL = 'http://localhost:7865/';
+  const options = { 
+	  url: API_URL,
+	  method: 'GET'
+	}
   it('GET / returns correct response', (done) => {
-    request.get(`${API_URL}/`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Welcome to the payment system');
+    request(options, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
       done();
     });
   });
-});
+  it('GET / returns correct response', (done) => {
+	  request(options, (err, res, body) => {
+		  expect(body).to.contain('Welcome to the payment system');
+		  done();
+	  });
+  });
+
+})
