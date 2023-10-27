f0a3a3c485b721e1f5b557133a4bb5be8c404363
Pascal-Hub <sequencepascal@gmail.com>
2023-10-27 12:47:33 +0000

    promis_taske

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,9 @@
-function handleResponseFromAPI(promise) {
-  return promise
-    .then((response) => {
-      console.log('Got a response from the API'); // Log the success message
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch((error) => {
-      console.log('Got a response from the API'); // Log the error message
-      return new Error();
+export default function handleResponseFromAPI(resolvedPromise) {
+  const newPromise = resolvedPromise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => {
+      console.log('Got a response from the API');
     });
+  return newPromise;
 }
-
-export default handleResponseFromAPI;
